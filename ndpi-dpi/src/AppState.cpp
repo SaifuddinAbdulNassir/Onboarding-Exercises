5121155f78e3e58753868b6b924516686f30f2df
Saifuddin Abdul Nassir <seyyunas@gmail.com>
2026-01-29 19:59:57 +0000

    Keeping initializer list in seperate line

--- ndpi-dpi/src/AppState.cpp
+++ ndpi-dpi/src/AppState.cpp
@@ -4,7 +4,8 @@ using namespace ndpi;
 
 // Constructors & destructors
 
-AppState::AppState() : maxPackets(100), running(true), uid(1)
+AppState::AppState()
+    : maxPackets(100), running(true), uid(1)
 {
 }
 
