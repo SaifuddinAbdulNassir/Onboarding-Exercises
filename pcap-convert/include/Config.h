6ccd2eb8c4acf3bf9b6d0fc53693706464c3c937
Saifuddin Abdul Nassir <seyyunas@gmail.com>
2026-01-30 15:36:31 +0000

    Namespace std removed and formatting of inline functions done.

--- pcap-convert/include/Config.h
+++ pcap-convert/include/Config.h
@@ -3,43 +3,41 @@
 // Standard includes
 #include <string>
 
-using namespace std;
-
 namespace pcapconvert
 {
 
-  class Config
-  {  
-    private:
+class Config
+{
+  private:
     // Data
-      string dnsAddr;
-      int dnsPort = -1;
-      string inputFile;
-      int ipVersion = -1;
-      string outputFile;
-      int ttlDec = -1;
-      int vlan = -1;
+    std::string dnsAddr;
+    int dnsPort = -1;
+    std::string inputFile;
+    int ipVersion = -1;
+    std::string outputFile;
+    int ttlDec = -1;
+    int vlan = -1;
 
-    public:
-      // Constructor and Destructor
-      Config();
-      ~Config();
-  
-      // Getters and Setters
-      string getDnsAddr() const {return dnsAddr;}
-      int getDnsPort() const {return dnsPort;}
-      string getInputFile() const {return inputFile;}
-      int getIpVersion() const {return ipVersion;}
-      string getOutputFile() const {return outputFile;}
-      int getTtlDec() const {return ttlDec;}
-      int getVlan() const {return vlan;}
-      void setDnsAddr(const string &addr) {dnsAddr = addr;}
-      void setDnsPort(int port) {dnsPort = port;}
-      void setInputFile(const string &file) {inputFile = file;}
-      void setIpVersion(int version) {ipVersion = version;}
-      void setOutputFile(const string &file) {outputFile = file;}
-      void setTtlDec(int dec) {ttlDec = dec;}
-      void setVlan(int id) {vlan = id;}
-  };
+  public:
+    // Constructor and Destructor
+    Config();
+    ~Config();
+
+    // Getters and Setters
+    std::string getDnsAddr() const { return dnsAddr; }
+    int getDnsPort() const { return dnsPort; }
+    std::string getInputFile() const { return inputFile; }
+    int getIpVersion() const { return ipVersion; }
+    std::string getOutputFile() const { return outputFile; }
+    int getTtlDec() const { return ttlDec; }
+    int getVlan() const { return vlan; }
+    void setDnsAddr(const std::string& addr) { dnsAddr = addr; }
+    void setDnsPort(int port) { dnsPort = port; }
+    void setInputFile(const std::string& file) { inputFile = file; }
+    void setIpVersion(int version) { ipVersion = version; }
+    void setOutputFile(const std::string& file) { outputFile = file; }
+    void setTtlDec(int dec) { ttlDec = dec; }
+    void setVlan(int id) { vlan = id; }
+};
 
 } // namespace pcapconvert
