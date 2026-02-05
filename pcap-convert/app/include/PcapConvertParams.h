c37a38e5212f921b2ca36e4cf7dafa0f3fd0f953
Saifuddin Abdul Nassir <seyyunas@gmail.com>
2026-02-05 23:01:45 +0000

    Change order of getters and setters

--- pcap-convert/app/include/PcapConvertParams.h
+++ pcap-convert/app/include/PcapConvertParams.h
@@ -31,18 +31,18 @@ namespace pcapconvert
 
     // Getters & setters
     std::variant<pcpp::IPv4Address, pcpp::IPv6Address> *getDnsAddr() const { return dnsAddr.get(); }
-    uint16_t *getDnsPort() const { return dnsPort.get(); }
-    const std::string &getInputFile() const { return inputFile; }
-    const pcpp::IPAddress::AddressType &getIpVersion() const { return ipVersion; }
-    const std::string &getOutputFile() const { return outputFile; }
-    uint8_t *getTtlDec() const { return ttlDec.get(); }
-    uint16_t *getVlan() const { return vlan.get(); }
     void setDnsAddr(const std::string &addr);
+    uint16_t *getDnsPort() const { return dnsPort.get(); }
     void setDnsPort(std::unique_ptr<uint16_t> port) { dnsPort = std::move(port); }
+    const std::string &getInputFile() const { return inputFile; }
     void setInputFile(const std::string &file) { inputFile = file; }
+    const pcpp::IPAddress::AddressType &getIpVersion() const { return ipVersion; }
     void setIpVersion(const pcpp::IPAddress::AddressType &version) { ipVersion = version; }
+    const std::string &getOutputFile() const { return outputFile; }
     void setOutputFile(const std::string &file) { outputFile = file; }
+    uint8_t *getTtlDec() const { return ttlDec.get(); }
     void setTtlDec(std::unique_ptr<uint8_t> dec) { ttlDec = std::move(dec); }
+    uint16_t *getVlan() const { return vlan.get(); }
     void setVlan(std::unique_ptr<uint16_t> id) { vlan = std::move(id); }
   };
 
